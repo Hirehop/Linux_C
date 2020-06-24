e567befb6f42b5ef9ed2adf7f87c246a0c5649ee
Xiang Xv <41570874+Hirehop@users.noreply.github.com>
2020-06-24 23:06:38 +0800

    Add files via upload

---
+++ Login/clientValidate.c
@@ -0,0 +1,56 @@
+// 本地通讯
+// IP地址使用本地环回口IP:127.0.0.1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "myMessageProtocol.h"
+#define IP "127.0.0.1"
+
+#define PORT 10085
+
+
+// 参数一：传输文件描述符
+// 参数二：用户传入结构体，内部封装了用户的id和password，发往服务端进行验证
+// 返回值：userInfo结构体
+//    成功user.username为实际值
+//    失败user.username为""
+struct userInfo client_process(int ss,struct userInfo user){
+    write(ss,&user,sizeof(user));
+    read(ss,&user,sizeof(user));
+    return user;
+}
+
+
+// 验证处理主函数
+// 参数：用户传入的userInfo结构体
+// 返回值：
+//     userInfo结构体[依赖内部处理函数client_process]
+struct userInfo clientValidate(struct userInfo user){
+    int ss;
+    struct sockaddr_in server_addr;
+ 
+    ss = socket(AF_INET,SOCK_STREAM,0);
+    if(ss == -1){
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    memset(&server_addr,0,sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr(IP);
+ 
+    //inet_pton(AF_INET,argv[1],&server_addr.sin_addr);
+ 
+    connect(ss,(struct sockaddr*)&server_addr,sizeof(server_addr));
+    user = client_process(ss,user);
+    close(ss);
+    return user;
+}
+
+
---
+++ Login/menuLogin.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "consoleLocation.h"
+#include "myMessageProtocol.h"
+
+/*
+struct userInfo{
+    char id[12];
+    char username[12];
+    char password[12];
+};
+*/
+
+// scanKeyboard.o中的两个方法
+int setDisplayMode(int, int);
+int scanKeyboard();
+
+// requireIdToP2P.o
+struct userInfo showP2PRequire();
+
+
+void showLoginMenu_temp(char *username){
+    // CLEAR();
+    system("clear");
+    char welcomeInfo[128];
+    MOVETO(2,0);
+    sprintf(welcomeInfo,"   =====Welcome[%s]=====\n",username);
+    printf("%s",welcomeInfo);
+    MOVERIGHT(3);
+    printf("1:P2P Communication\n");
+    MOVERIGHT(3);
+    printf("2:Group Communication\n");
+    MOVERIGHT(3);
+    printf("3:Logout\n");
+}
+
+
+// 本方法中包含了当前用户信息，和待处理的对方用户信息
+// 执行用户的选择
+int showMenuLogin(struct userInfo user){
+    aaa:
+    HIDE_CURSOR();
+    char username[12];
+    strcpy(username,user.username);
+    showLoginMenu_temp(username);
+
+    int choice = 1;
+    int line = 3;
+    MOVETO(line,0);
+    printf("->\n");
+
+    setDisplayMode(0,0);
+
+    for(;;){
+        int res = scanKeyboard();
+        switch(res){
+            case 119:
+                if(line<=3){
+                    line = 3;
+                    choice = 1;
+                }else{
+                    line--;
+                    choice--;
+                }
+                MOVETO(2,0);
+                showLoginMenu_temp(username);
+                MOVETO(line,0);
+                printf("->\n");
+                break;
+            case 115:
+                if(line>=5){
+                    line=5;
+                    choice = 3;
+                }else{
+                    line++;
+                    choice++;
+                }
+                MOVETO(2,0);
+                showLoginMenu_temp(username);
+                MOVETO(line,0);
+                printf("->\n");
+                break;
+            default:
+                MOVETO(2,0);
+        }
+        if(res==10){
+            if(choice==1){
+                // 转到聊天界面
+                system("clear");
+                MOVETO(2,0);
+                printf("==========Communicating==========\n");
+                printf("Format:[oppositeId messageContent]\n");
+
+                // 将本地用户登录信息传入通讯模块
+                clientP2PComminicate(user);
+                
+
+            }else if(choice==2){
+            
+            }else if(choice==3){
+                
+            }
+        }
+    }
+}
